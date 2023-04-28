d105cfe540155b50cb8bb4b0e73dc990f47afbdb
wada <adela.wolfova@gmail.com>
2023-04-28 12:05:24 +0200

    Learn how to replace string, how to use string methods, how to use HTML events

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function run() {
-    document.body.style.background = 'red';
+    document.body.style.background = '#50b8f5';
 }
 
+function pinkColor() {
+    document.body.style.background = 'rgb(226, 43, 165)';
+}
+
+
+function mimi() {
+    document.body.style.background = 'rgb(10, 214, 102)';
+}
+
+
+
+function replaceFunction() {
+    let text = document.getElementById("replaceString").innerHTML;
+    document.getElementById("replaceString").innerHTML = text.replace("Jasmine","Sarah");
+}
